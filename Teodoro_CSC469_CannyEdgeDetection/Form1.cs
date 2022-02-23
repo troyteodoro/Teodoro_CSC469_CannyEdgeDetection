661a14c8563491c5c3f9bab2b6eef5824476a27e
Troy Teodoro <teodoro@unlv.nevada.edu>
2022-02-23 11:46:36 -0800

    implemented calcnonmaximalsuppresion WIP drawBitmap

--- Teodoro_CSC469_CannyEdgeDetection/Form1.cs
+++ Teodoro_CSC469_CannyEdgeDetection/Form1.cs
@@ -143,7 +143,7 @@ namespace Teodoro_CSC469_CannyEdgeDetection
 
         }
 
-        public void calcGradients(Bitmap original, float[,] gradientX, float[,] gradientY)
+        public void calcGradients(Bitmap original, float[,] gradient, float[,] gradientX, float[,] gradientY)
         {
             for(int i = 1; i < original.Width-3; i++)
             {
@@ -151,13 +151,94 @@ namespace Teodoro_CSC469_CannyEdgeDetection
                 {
                     gradientX[i, j] = (float)((original.GetPixel(i - 1, j)).A - (original.GetPixel(i + 1, j)).A) / 2;
                     gradientY[i, j] = (float)((original.GetPixel(i, j - 1)).A - (original.GetPixel(i, j + 1)).A) / 2;
+                    gradient[i, j] = gradientX[i, j] + gradientY[i, j];
                 }
             }
         }
-        //public Bitmap calcNonMaximalSuppression(Bitmap original)
-        //{
-        //
-        //}
+        public void calcNonMaximalSuppression(Bitmap original, float[,] gradientX, float[,] gradientY, float[,] edgeDirection)
+        {
+            const double PI = 3.14159;
+            float currAngle;
+            decimal temp;
+            double tempX, tempY;
+            for(int i = 1; i < original.Width-2; i++)
+            {
+                for(int j = 1; j < original.Height-2; j++)
+                {
+                    //Convert float to double
+                    temp = new decimal(gradientX[i, j]);
+                    tempX = (double)temp;
+                    temp = new decimal(gradientY[i, j]);
+                    tempY = (double)temp;
+
+                    //Convert to Dec
+                    currAngle = (float)(Math.Atan2(tempY, tempX) / PI) * 180; //Convert to Dec
+
+                    //Determine Non-maximal Suppression
+                    if ((currAngle < 22.5 && currAngle > -22.5) || (currAngle > 157.5) || currAngle < -157.5 )
+                    {
+                        edgeDirection[i, j] = 0;
+                    }else if ((currAngle > 22.5 && currAngle < 67.5) || (currAngle < -112.5) || currAngle > -157.5 )
+                    {
+                        edgeDirection[i, j] = 45;
+                    }else if ((currAngle > 67.5 && currAngle < 112.5) || (currAngle < -67.5) || currAngle > -112.5 )
+                    {
+                        edgeDirection[i, j] = 90;
+                    }else if ((currAngle > 112.5 && currAngle < 157.5) || (currAngle < -22.5) || currAngle > -67.5 )
+                    {
+                        edgeDirection[i, j] = 135;
+                    }
+                }
+            }
+        }
+
+        public Bitmap drawBitmap(Bitmap original, float[,] pixelAngle, float[,] gradient, int upperThreshold, int lowerThreshold)
+        {
+            bool isEdgeEnd = false;
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+            Color background = new Color();
+            background = Color.FromArgb(0,0,0,0);
+            for(int i = 1; i < original.Width-2; i++)
+            {
+                for(int j = 1; j < original.Height-2; j++)
+                {
+                    isEdgeEnd = false;
+                    if(gradient[i,j] > upperThreshold)
+                    {
+                        switch(pixelAngle[i,j])
+                        {
+                            case 0:
+                                newBitmap = findEdge(original, newBitmap, 0, 1, i, j, 0, lowerThreshold);
+                                break;
+                            case 45:
+                                newBitmap = findEdge(original, newBitmap, 1, 1, i, j, 45, lowerThreshold);
+                                break;
+                            case 90:
+                                newBitmap = findEdge(original, newBitmap, 1, 0, i, j, 90, lowerThreshold);
+                                break;
+                            case 135:
+                                newBitmap = findEdge(original, newBitmap, 1, -1, i, j, 135, lowerThreshold);
+                                break;
+                            default:
+                                //make black
+                                newBitmap.SetPixel(i, j, background);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        newBitmap.SetPixel(i,j, background);
+                    }
+                }
+            }
+            return newBitmap;
+        }
+
+        private Bitmap findEdge(Bitmap original, Bitmap newBitmap, int rowShift, int colShift, int row, int col, int dir, int lowerThreshold)
+        {
+
+            return newBitmap;
+        }
         //
         //public Bitmap calcThresholdingHysterias(Bitmap orginal)
         //{
@@ -184,12 +265,22 @@ namespace Teodoro_CSC469_CannyEdgeDetection
             Bitmap newImage = calcSmoothing(original);
 
             //Find Gradient
-            float[,] gradientX = new float[original.Width, original.Height];
-            float[,] gradientY = new float[original.Width, original.Height];
-            calcGradients(original, gradientX, gradientY);
+            float[,] gradient = new float[newImage.Width, newImage.Height];
+            float[,] gradientX = new float[newImage.Width, newImage.Height];
+            float[,] gradientY = new float[newImage.Width, newImage.Height];
+            calcGradients(newImage, gradient, gradientX, gradientY);
 
             //Non-maximal Suppression
+            float[,] pixelAngle = new float[newImage.Width, newImage.Height]; 
+            calcNonMaximalSuppression(newImage, gradientX, gradientY, pixelAngle);
+           
+            //Draw Bitmap
+            int upperThreshold, lowerThreshold;
+ 
+
             //Thresholding Hysterias
+
+            //Display 
             PictureBox cannyPictureBox = new PictureBox();
             cannyPictureBox.Size = newImage.Size;
             cannyPictureBox.Image = newImage;
